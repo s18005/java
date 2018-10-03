3840050ff69723b80cf19d098bc7b3457cd6edf3
ryuma <s18005@std.it-college.ac.jp>
2018-10-03 07:22:18 +0000

    java kadai

---
+++ 1003/En2_2.java
@@ -0,0 +1,11 @@
+class sub {
+    public static void main(String[] args) {
+	for (int i = 1; i <= 100; ++i) {
+	    if (i % 3 == 0 || Integer.toString(i).contains("3")) {
+		System.out.println("THREE");
+	    } else {
+		System.out.println(i);
+	    }
+        }
+    }
+}
