ff2dbe8258bcb0e72b0c5263c7c39904051d0e20
ryuma <s18005@std.it-college.ac.jp>
2018-10-18 07:32:13 +0000

    java kadai

---
+++ 1018/En7_1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class En7_1 {
+    public static void main(String[] args) {
+	List<Integer> dtut = new ArrayList<>();
+	dtut.add(2);
+	dtut.add(3);
+	dtut.add(1);
+	dtut.add(2);
+
+	System.out.println(dtut);
+    }
+}
---
+++ 1018/En7_2.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+import java.util.Set;
+
+public class En7_2 {
+    public static void main(String[] args) {
+	Set<Integer> dtut = new TreeSet<>();
+	dtut.add(2);
+	dtut.add(3);
+	dtut.add(1);
+	dtut.add(2);
+
+	System.out.println(dtut);
+    }
+}
